6a1490e44ab1518c0e351cf2d025d64d9455ed89
Jutho Haegeman <jutho.haegeman@ugent.be>
2025-04-20 23:41:12 +0200

    small triangular! fix

--- src/common/initialization.jl
+++ src/common/initialization.jl
@@ -10,3 +10,23 @@ function one!(A::AbstractMatrix)
     diagview(A) .= one(eltype(A))
     return A
 end
+
+function uppertriangular!(A::AbstractMatrix)
+    Base.require_one_based_indexing(A)
+    m, n = size(A)
+    for i in 1:n
+        r = (i + 1):m
+        zero!(view(A, r, i))
+    end
+    return A
+end
+
+function lowertriangular!(A::AbstractMatrix)
+    Base.require_one_based_indexing(A)
+    m, n = size(A)
+    for i in 2:n
+        r = 1:min(i - 1, m)
+        zero!(view(A, r, i))
+    end
+    return A
+end
