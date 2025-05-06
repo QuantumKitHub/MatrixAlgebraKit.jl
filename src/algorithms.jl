df4b2b84f9fb91eb4c570e9c9a1e3457127aba4a
mtfishman <mfishman@flatironinstitute.org>
2025-05-06 14:05:59 -0400

    Add more tests

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -72,17 +72,20 @@ can't be passed to `MatrixAlgebraKit.select_algorithm`.
 """
 function select_algorithm end
 
-Base.@constprop :aggressive function select_algorithm(f::F, A, alg::Alg=nothing; kwargs...) where {F,Alg}
+Base.@constprop :aggressive function select_algorithm(f::F, A, alg::Alg=nothing;
+                                                      kwargs...) where {F,Alg}
     return _select_algorithm(f, A, alg; kwargs...)
 end
 
 function _select_algorithm(f::F, A, alg::Nothing; kwargs...) where {F}
     return default_algorithm(f, A; kwargs...)
 end
-Base.@constprop :aggressive function _select_algorithm(f::F, A, alg::Symbol; kwargs...) where {F}
+Base.@constprop :aggressive function _select_algorithm(f::F, A, alg::Symbol;
+                                                       kwargs...) where {F}
     return Algorithm{alg}(; kwargs...)
 end
-Base.@constprop :aggressive function _select_algorithm(f::F, A, ::Type{Alg}; kwargs...) where {F,Alg}
+Base.@constprop :aggressive function _select_algorithm(f::F, A, ::Type{Alg};
+                                                       kwargs...) where {F,Alg}
     return Alg(; kwargs...)
 end
 function _select_algorithm(f::F, A, alg::NamedTuple; kwargs...) where {F}
@@ -174,7 +177,8 @@ macro functiondef(f)
 
     return esc(quote
                    # out of place to inplace
-                   Base.@constprop :aggressive $f(A; kwargs...) = $f!(copy_input($f, A); kwargs...)
+                   Base.@constprop :aggressive $f(A; kwargs...) = $f!(copy_input($f, A);
+                                                                      kwargs...)
                    $f(A, alg::AbstractAlgorithm) = $f!(copy_input($f, A), alg)
 
                    # fill in arguments
