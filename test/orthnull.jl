f7c324bc15cf689dfe2f9f9217afa224e3170252
Lukas Devos <ldevos98@gmail.com>
2025-05-27 09:46:44 -0400

    fix tests

--- test/orthnull.jl
+++ test/orthnull.jl
@@ -4,7 +4,7 @@ using TestExtras
 using StableRNGs
 using LinearAlgebra: LinearAlgebra, I, mul!
 using MatrixAlgebraKit: TruncationKeepAbove, TruncationKeepBelow
-using MatrixAlgebraKit: LAPACK_SVDAlgorithm, check_input, copy_input, default_svd_algorithm,
+using MatrixAlgebraKit: LAPACK_SVDAlgorithm, check_input, copy_input, default_algorithm,
                         initialize_output
 
 # Used to test non-AbstractMatrix codepaths.
@@ -39,8 +39,9 @@ end
 function MatrixAlgebraKit.check_input(::typeof(right_orth!), A::LinearMap, VC)
     return check_input(right_orth!, parent(A), parent.(VC))
 end
-function MatrixAlgebraKit.default_svd_algorithm(A::LinearMap)
-    return default_svd_algorithm(parent(A))
+function MatrixAlgebraKit.default_algorithm(::typeof(svd_compact!),
+                                            ::Type{LinearMap{A}}) where {A}
+    return default_algorithm(svd_compact!, A)
 end
 function MatrixAlgebraKit.initialize_output(::typeof(svd_compact!), A::LinearMap,
                                             alg::LAPACK_SVDAlgorithm)
