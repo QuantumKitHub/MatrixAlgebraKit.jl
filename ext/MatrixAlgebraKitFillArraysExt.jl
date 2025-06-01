1701e164df5704fa55732f326c342efc4e7a5e17
mtfishman <mfishman@flatironinstitute.org>
2025-06-01 19:49:55 -0400

    More functionality and tests

--- ext/MatrixAlgebraKitFillArraysExt.jl
+++ ext/MatrixAlgebraKitFillArraysExt.jl
@@ -2,12 +2,20 @@ module MatrixAlgebraKitFillArraysExt
 
 using LinearAlgebra
 using MatrixAlgebraKit
-using MatrixAlgebraKit: AbstractAlgorithm
+using MatrixAlgebraKit: AbstractAlgorithm, check_input
 using FillArrays
 using FillArrays: AbstractZerosMatrix
 
-struct EyeAlgorithm <: AbstractAlgorithm end
 struct ZerosAlgorithm <: AbstractAlgorithm end
+
+for f in [:eig, :eigh, :lq, :qr, :svd]
+    ff = Symbol("default_", f, "_algorithm")
+    @eval begin
+        function MatrixAlgebraKit.$ff(::Type{<:AbstractZerosMatrix}; kwargs...)
+            return ZerosAlgorithm()
+        end
+    end
+end
 
 for f in [:eig_full,
           :eigh_full,
@@ -19,81 +27,181 @@ for f in [:eig_full,
           :right_polar,
           :svd_compact,
           :svd_full]
+    f! = Symbol(f, "!")
     @eval begin
-        MatrixAlgebraKit.copy_input(::typeof($f), a::Eye) = a
-
-        MatrixAlgebraKit.copy_input(::typeof($f), a::AbstractZerosMatrix) = a
-    end
-end
-
-for f in [:eig, :eigh, :lq, :qr, :polar, :svd]
-    ff = Symbol("default_", f, "_algorithm")
-    @eval begin
-        function MatrixAlgebraKit.$ff(a::Type{<:Eye}; kwargs...)
-            return EyeAlgorithm()
-        end
-
-        function MatrixAlgebraKit.$ff(a::Type{<:AbstractZerosMatrix}; kwargs...)
-            return ZerosAlgorithm()
-        end
-    end
-end
-
-for f in [:eig_full!,
-          :eigh_full!,
-          :qr_compact!,
-          :qr_full!,
-          :left_polar!,
-          :lq_compact!,
-          :lq_full!,
-          :right_polar!]
-    @eval begin
-        nfactors(::typeof($f)) = 2
-    end
-end
-for f in [:svd_compact!, :svd_full!]
-    @eval begin
-        nfactors(::typeof($f)) = 3
-    end
-end
-
-for f in [:eig_full!,
-          :eigh_full!,
-          :qr_compact!,
-          :qr_full!,
-          :left_polar!,
-          :lq_compact!,
-          :lq_full!,
-          :right_polar!,
-          :svd_compact!,
-          :svd_full!]
-    @eval begin
-        function MatrixAlgebraKit.initialize_output(::typeof($f), a::Eye,
-                                                    alg::EyeAlgorithm)
-            return nothing
-        end
-        function MatrixAlgebraKit.check_input(::typeof($f), A::Eye, F)
-            LinearAlgebra.checksquare(A)
-            return nothing
-        end
-
-        function MatrixAlgebraKit.$f(a::Eye, F, alg::EyeAlgorithm; kwargs...)
-            return ntuple(_ -> a, nfactors($f))
-        end
-
-        function MatrixAlgebraKit.initialize_output(::typeof($f), a::AbstractZerosMatrix,
+        MatrixAlgebraKit.copy_input(::typeof($f), A::AbstractZerosMatrix) = A
+        function MatrixAlgebraKit.initialize_output(::typeof($f!), A::AbstractZerosMatrix,
                                                     alg::ZerosAlgorithm)
             return nothing
         end
+    end
+end
+
+for f in [:eig_full!,
+          :eigh_full!]
+    @eval begin
         function MatrixAlgebraKit.check_input(::typeof($f), A::AbstractZerosMatrix, F)
             LinearAlgebra.checksquare(A)
             return nothing
         end
-        function MatrixAlgebraKit.$f(a::AbstractZerosMatrix, F, alg::ZerosAlgorithm;
+        function MatrixAlgebraKit.$f(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm;
                                      kwargs...)
-            return ntuple(_ -> a, nfactors($f))
+            check_input($f, A, F)
+            return (A, Eye(axes(A)))
         end
     end
+end
+
+function MatrixAlgebraKit.qr_compact!(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm)
+    m, n = size(A)
+    ax = axes(A)
+    if m > n
+        r_ax = (ax[2], ax[2])
+        return (Eye(ax), Zeros(r_ax))
+    else
+        q_ax = (ax[1], ax[1])
+        return (Eye(q_ax), Zeros(ax))
+    end
+end
+
+function MatrixAlgebraKit.qr_full!(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm)
+    ax = axes(A)
+    q_ax = (ax[1], ax[1])
+    return (Eye(q_ax), Zeros(ax))
+end
+
+function MatrixAlgebraKit.lq_compact!(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm)
+    m, n = size(A)
+    ax = axes(A)
+    if m < n
+        l_ax = (ax[1], ax[1])
+        return (Zeros(l_ax), Eye(ax))
+    else
+        q_ax = (ax[2], ax[2])
+        return (Zeros(ax), Eye(q_ax))
+    end
+end
+
+function MatrixAlgebraKit.lq_full!(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm)
+    ax = axes(A)
+    q_ax = (ax[2], ax[2])
+    return (Zeros(ax), Eye(q_ax))
+end
+
+function MatrixAlgebraKit.svd_compact!(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm)
+    m, n = size(A)
+    ax = axes(A)
+    if m > n
+        s_ax = (ax[2], ax[2])
+        return (Eye(ax), Zeros(s_ax), Eye(s_ax))
+    else
+        s_ax = (ax[1], ax[1])
+        return (Eye(s_ax), Zeros(s_ax), Eye(ax))
+    end
+end
+
+function MatrixAlgebraKit.svd_full!(A::AbstractZerosMatrix, F, alg::ZerosAlgorithm)
+    ax = axes(A)
+    return (Eye((ax[1], ax[1])), Zeros(ax), Eye((ax[2], ax[2])))
+end
+
+struct EyeAlgorithm <: AbstractAlgorithm end
+
+for f in [:eig, :eigh, :lq, :qr, :polar, :svd]
+    ff = Symbol("default_", f, "_algorithm")
+    @eval begin
+        function MatrixAlgebraKit.$ff(A::Type{<:Eye}; kwargs...)
+            return EyeAlgorithm()
+        end
+    end
+end
+
+for f in [:eig_full,
+          :eigh_full,
+          :qr_compact,
+          :qr_full,
+          :lq_compact,
+          :lq_full,
+          :left_polar,
+          :right_polar,
+          :svd_compact,
+          :svd_full]
+    f! = Symbol(f, "!")
+    @eval begin
+        MatrixAlgebraKit.copy_input(::typeof($f), A::Eye) = A
+        function MatrixAlgebraKit.initialize_output(::typeof($f!), A::Eye,
+                                                    alg::EyeAlgorithm)
+            return nothing
+        end
+    end
+end
+
+for f in [:eig_full!,
+          :eigh_full!]
+    @eval begin
+        function MatrixAlgebraKit.check_input(::typeof($f), A::Eye, F)
+            LinearAlgebra.checksquare(A)
+            return nothing
+        end
+        function MatrixAlgebraKit.$f(A::Eye, F, alg::EyeAlgorithm;
+                                     kwargs...)
+            check_input($f, A, F)
+            return (A, A)
+        end
+    end
+end
+
+function MatrixAlgebraKit.qr_compact!(A::Eye, F, alg::EyeAlgorithm)
+    m, n = size(A)
+    ax = axes(A)
+    if m > n
+        r_ax = (ax[2], ax[2])
+        return (Eye(ax), Eye(r_ax))
+    else
+        q_ax = (ax[1], ax[1])
+        return (Eye(q_ax), Eye(ax))
+    end
+end
+
+function MatrixAlgebraKit.qr_full!(A::Eye, F, alg::EyeAlgorithm)
+    ax = axes(A)
+    q_ax = (ax[1], ax[1])
+    return (Eye(q_ax), A)
+end
+
+function MatrixAlgebraKit.lq_compact!(A::Eye, F, alg::EyeAlgorithm)
+    m, n = size(A)
+    ax = axes(A)
+    if m < n
+        l_ax = (ax[1], ax[1])
+        return (Eye(l_ax), Eye(ax))
+    else
+        q_ax = (ax[2], ax[2])
+        return (Eye(ax), Eye(q_ax))
+    end
+end
+
+function MatrixAlgebraKit.lq_full!(A::Eye, F, alg::EyeAlgorithm)
+    ax = axes(A)
+    q_ax = (ax[2], ax[2])
+    return (A, Eye(q_ax))
+end
+
+function MatrixAlgebraKit.svd_compact!(A::Eye, F, alg::EyeAlgorithm)
+    m, n = size(A)
+    ax = axes(A)
+    if m > n
+        s_ax = (ax[2], ax[2])
+        return (Eye(ax), Eye(s_ax), Eye(s_ax))
+    else
+        s_ax = (ax[1], ax[1])
+        return (Eye(s_ax), Eye(s_ax), Eye(ax))
+    end
+end
+
+function MatrixAlgebraKit.svd_full!(A::Eye, F, alg::EyeAlgorithm)
+    ax = axes(A)
+    return (Eye((ax[1], ax[1])), A, Eye((ax[2], ax[2])))
 end
 
 end
--- test/fillarrays.jl
+++ test/fillarrays.jl
@@ -1,31 +1,185 @@
 using MatrixAlgebraKit
+using LinearAlgebra
 using Test
 using TestExtras
 using FillArrays
 
-@testset "Eye" begin
-    for f in [:eig_full!,
-              :eigh_full!,
-     # TODO: Reenable once https://github.com/QuantumKitHub/MatrixAlgebraKit.jl/pull/32
-     # is merged.
-     # :qr_compact!,
-     # :qr_full!,
-              :left_polar!,
-              :lq_compact!,
-              :lq_full!,
-              :right_polar!,
-              :svd_compact!,
-              :svd_full!]
+@testset "Zeros" begin
+    for f in [:eig_full, :eigh_full]
         @eval begin
-            A = Eye(3)
-            F = @constinferred $f(A)
-            @test A === prod(F)
-            @test all(x -> x === A, F)
-
             A = Zeros(3, 3)
-            F = @constinferred $f(A)
-            @test A === prod(F)
-            @test all(x -> x === A, F)
+            D, V = @constinferred $f(A)
+            @test A * V == D * V
+            @test size(D) == size(A)
+            @test size(V) == size(A)
+            @test iszero(D)
+            @test D isa Zeros
+            @test V == I
+            @test V isa Eye
         end
     end
+
+    # for f in [:qr_compact, :left_polar]
+    #     @eval begin
+    #         A = Zeros(4, 3)
+    #         Q, R = @constinferred $f(A)
+    #         @test A == Q * R
+    #         @test size(Q) == (4, 3)
+    #         @test size(R) == (3, 3)
+    #         @test Q == Matrix(I, (4, 3))
+    #         @test Q isa Eye
+    #         @test iszero(R)
+    #         @test R isa Zeros
+    #     end
+    # end
+
+    # A = Zeros(4, 3)
+    # Q, R = @constinferred qr_full(A)
+    # @test A == Q * R
+    # @test size(Q) == (4, 4)
+    # @test size(R) == (4, 3)
+    # @test Q == I
+    # @test Q isa Eye
+    # @test iszero(R)
+    # @test R isa Zeros
+
+    for f in [:lq_compact] # :right_polar]
+        @eval begin
+            A = Zeros(3, 4)
+            L, Q = @constinferred $f(A)
+            @test A == L * Q
+            @test size(L) == (3, 3)
+            @test size(Q) == (3, 4)
+            @test iszero(L)
+            @test L isa Zeros
+            @test Q == Matrix(I, (3, 4))
+            @test Q isa Eye
+        end
+    end
+
+    A = Zeros(3, 4)
+    L, Q = @constinferred lq_full(A)
+    @test A == L * Q
+    @test size(L) == (3, 4)
+    @test size(Q) == (4, 4)
+    @test iszero(L)
+    @test L isa Zeros
+    @test Q == I
+    @test Q isa Eye
+
+    A = Zeros(3, 4)
+    U, S, V = @constinferred svd_compact(A)
+    @test U * S * V == A
+    @test size(U) == (3, 3)
+    @test size(S) == (3, 3)
+    @test size(V) == (3, 4)
+    @test iszero(S)
+    @test S isa Zeros
+    @test U == I
+    @test U isa Eye
+    @test V == Matrix(I, (3, 4))
+    @test V isa Eye
+
+    A = Zeros(3, 4)
+    U, S, V = @constinferred svd_full(A)
+    @test U * S * V == A
+    @test size(U) == (3, 3)
+    @test size(S) == (3, 4)
+    @test size(V) == (4, 4)
+    @test iszero(S)
+    @test S isa Zeros
+    @test U == I
+    @test U isa Eye
+    @test V == I
+    @test V isa Eye
+end
+
+@testset "Eye" begin
+    for f in [:eig_full, :eigh_full]
+        @eval begin
+            A = Eye(3, 3)
+            D, V = @constinferred $f(A)
+            @test A * V == D * V
+            @test size(D) == size(A)
+            @test size(V) == size(A)
+            @test V == I
+            @test D isa Eye
+            @test V == I
+            @test V isa Eye
+        end
+    end
+
+    # for f in [:qr_compact, :left_polar]
+    #     @eval begin
+    #         A = Eye(4, 3)
+    #         Q, R = @constinferred $f(A)
+    #         @test A == Q * R
+    #         @test size(Q) == (4, 3)
+    #         @test size(R) == (3, 3)
+    #         @test Q == Matrix(I, (4, 3))
+    #         @test Q isa Eye
+    #         @test R == I
+    #         @test R isa Eye
+    #     end
+    # end
+
+    # A = Eye(4, 3)
+    # Q, R = @constinferred qr_full(A)
+    # @test A == Q * R
+    # @test size(Q) == (4, 4)
+    # @test size(R) == (4, 3)
+    # @test Q == I
+    # @test Q isa Eye
+    # @test R == I
+    # @test R isa Eye
+
+    for f in [:lq_compact] # :right_polar]
+        @eval begin
+            A = Eye(3, 4)
+            L, Q = @constinferred $f(A)
+            @test A == L * Q
+            @test size(L) == (3, 3)
+            @test size(Q) == (3, 4)
+            @test L == I
+            @test L isa Eye
+            @test Q == Matrix(I, (3, 4))
+            @test Q isa Eye
+        end
+    end
+
+    A = Eye(3, 4)
+    L, Q = @constinferred lq_full(A)
+    @test A == L * Q
+    @test size(L) == (3, 4)
+    @test size(Q) == (4, 4)
+    @test L == Matrix(I, (3, 4))
+    @test L isa Eye
+    @test Q == I
+    @test Q isa Eye
+
+    A = Eye(3, 4)
+    U, S, V = @constinferred svd_compact(A)
+    @test U * S * V == A
+    @test size(U) == (3, 3)
+    @test size(S) == (3, 3)
+    @test size(V) == (3, 4)
+    @test S == I
+    @test S isa Eye
+    @test U == I
+    @test U isa Eye
+    @test V == Matrix(I, (3, 4))
+    @test V isa Eye
+
+    A = Eye(3, 4)
+    U, S, V = @constinferred svd_full(A)
+    @test U * S * V == A
+    @test size(U) == (3, 3)
+    @test size(S) == (3, 4)
+    @test size(V) == (4, 4)
+    @test S == Matrix(I, (3, 4))
+    @test S isa Eye
+    @test U == I
+    @test U isa Eye
+    @test V == I
+    @test V isa Eye
 end
