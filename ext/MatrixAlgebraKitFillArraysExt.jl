9f63ee4f46b5ec9b2a680c055c912481b97e8d3f
mtfishman <mfishman@flatironinstitute.org>
2025-06-01 11:35:50 -0400

    FillArrays extension

---
+++ ext/MatrixAlgebraKitFillArraysExt.jl
@@ -0,0 +1,71 @@
+module MatrixAlgebraKitFillArraysExt
+
+using MatrixAlgebraKit
+using MatrixAlgebraKit: AbstractAlgorithm
+using FillArrays
+
+struct EyeAlgorithm <: AbstractAlgorithm end
+
+for f in [:eig_full,
+          :eigh_full,
+          :qr_compact,
+          :qr_full,
+          :left_polar,
+          :lq_compact,
+          :lq_full,
+          :right_polar,
+          :svd_compact,
+          :svd_full]
+    @eval begin
+        MatrixAlgebraKit.copy_input(::typeof($f), a::Eye) = a
+    end
+end
+
+for f in [:eig, :eigh, :lq, :qr, :polar, :svd]
+    ff = Symbol("default_", f, "_algorithm")
+    @eval begin
+        function MatrixAlgebraKit.$ff(a::Type{<:Eye}; kwargs...)
+            return EyeAlgorithm()
+        end
+    end
+end
+
+for f in [:eig_full!,
+          :eigh_full!,
+          :qr_compact!,
+          :qr_full!,
+          :left_polar!,
+          :lq_compact!,
+          :lq_full!,
+          :right_polar!]
+    @eval begin
+        nfactors(::typeof($f)) = 2
+    end
+end
+for f in [:svd_compact!, :svd_full!]
+    @eval begin
+        nfactors(::typeof($f)) = 3
+    end
+end
+
+for f in [:eig_full!,
+          :eigh_full!,
+          :qr_compact!,
+          :qr_full!,
+          :left_polar!,
+          :lq_compact!,
+          :lq_full!,
+          :right_polar!,
+          :svd_compact!,
+          :svd_full!]
+    @eval begin
+        function MatrixAlgebraKit.initialize_output(::typeof($f), a::Eye, alg::EyeAlgorithm)
+            return ntuple(_ -> a, nfactors($f))
+        end
+        function MatrixAlgebraKit.$f(a::Eye, F, alg::EyeAlgorithm; kwargs...)
+            return ntuple(_ -> a, nfactors($f))
+        end
+    end
+end
+
+end
---
+++ test/fillarrays.jl
@@ -0,0 +1,26 @@
+using MatrixAlgebraKit
+using Test
+using TestExtras
+using FillArrays
+
+@testset "Eye" begin
+    for f in [:eig_full!,
+              :eigh_full!,
+              # TODO: Reenable once https://github.com/QuantumKitHub/MatrixAlgebraKit.jl/pull/32
+              # is merged.
+              # :qr_compact!,
+              # :qr_full!,
+              :left_polar!,
+              :lq_compact!,
+              :lq_full!,
+              :right_polar!,
+              :svd_compact!,
+              :svd_full!]
+        @eval begin
+            A = Eye(3)
+            F = @constinferred $f(A)
+            @test A === prod(F)
+            @test all(x -> x === A, F)
+        end
+    end
+end
