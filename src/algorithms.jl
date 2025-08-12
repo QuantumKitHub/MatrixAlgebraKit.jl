57a448ab8d13c5ae924e767b4d6c5cfa35d9a194
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2025-08-12 11:54:49 +0200

    Split up the functiondef macro

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -157,6 +157,95 @@ macro algdef(name)
         end)
 end
 
+function _arg_expr(::Val{1}, f, f!)
+    return quote # out of place to inplace
+        $f(A; kwargs...) = $f!(copy_input($f, A); kwargs...)
+        $f(A, alg::AbstractAlgorithm) = $f!(copy_input($f, A), alg)
+
+        # fill in arguments
+        function $f!(A; alg=nothing, kwargs...)
+            return $f!(A, select_algorithm($f!, A, alg; kwargs...))
+        end
+        function $f!(A, out; alg=nothing, kwargs...)
+            return $f!(A, out, select_algorithm($f!, A, alg; kwargs...))
+        end
+        function $f!(A, alg::AbstractAlgorithm)
+            return $f!(A, initialize_output($f!, A, alg), alg)
+        end
+
+        # define fallbacks for algorithm selection
+        @inline function select_algorithm(::typeof($f), A, alg::Alg; kwargs...) where {Alg}
+            return select_algorithm($f!, A, alg; kwargs...)
+        end
+        # define default algorithm fallbacks for out-of-place functions
+        # in terms of the corresponding in-place function
+        @inline function default_algorithm(::typeof($f), A; kwargs...)
+            return default_algorithm($f!, A; kwargs...)
+        end
+        # define default algorithm fallbacks for out-of-place functions
+        # in terms of the corresponding in-place function for types,
+        # in principle this is covered by the definition above but
+        # it is necessary to avoid ambiguity errors with the generic definitions:
+        # ```julia
+        # default_algorithm(f::F, A; kwargs...) where {F} = default_algorithm(f, typeof(A); kwargs...)
+        # function default_algorithm(f::F, ::Type{T}; kwargs...) where {F,T}
+        #     throw(MethodError(default_algorithm, (f, T)))
+        # end
+        # ```
+        @inline function default_algorithm(::typeof($f), ::Type{A}; kwargs...) where {A}
+            return default_algorithm($f!, A; kwargs...)
+        end
+
+        # copy documentation to both functions
+        Core.@__doc__ $f, $f!
+    end
+end
+
+function _arg_expr(::Val{2}, f, f!)
+    return quote
+        # out of place to inplace
+        $f(A, B; kwargs...) = $f!(copy_input($f, A, B)...; kwargs...)
+        $f(A, B, alg::AbstractAlgorithm) = $f!(copy_input($f, A, B)..., alg)
+
+        # fill in arguments
+        function $f!(A, B; alg=nothing, kwargs...)
+            return $f!(A, B, select_algorithm($f!, (A, B), alg; kwargs...))
+        end
+        function $f!(A, B, out; alg=nothing, kwargs...)
+            return $f!(A, B, out, select_algorithm($f!, (A, B), alg; kwargs...))
+        end
+        function $f!(A, B, alg::AbstractAlgorithm)
+            return $f!(A, B, initialize_output($f!, A, B, alg), alg)
+        end
+
+        # define fallbacks for algorithm selection
+        @inline function select_algorithm(::typeof($f), A, alg::Alg; kwargs...) where {Alg}
+            return select_algorithm($f!, A, alg; kwargs...)
+        end
+        # define default algorithm fallbacks for out-of-place functions
+        # in terms of the corresponding in-place function
+        @inline function default_algorithm(::typeof($f), A, B; kwargs...)
+            return default_algorithm($f!, A, B; kwargs...)
+        end
+        # define default algorithm fallbacks for out-of-place functions
+        # in terms of the corresponding in-place function for types,
+        # in principle this is covered by the definition above but
+        # it is necessary to avoid ambiguity errors with the generic definitions:
+        # ```julia
+        # default_algorithm(f::F, A; kwargs...) where {F} = default_algorithm(f, typeof(A); kwargs...)
+        # function default_algorithm(f::F, ::Type{T}; kwargs...) where {F,T}
+        #     throw(MethodError(default_algorithm, (f, T)))
+        # end
+        # ```
+        @inline function default_algorithm(::typeof($f), ::Type{A}, ::Type{B}; kwargs...) where {A, B}
+            return default_algorithm($f!, A, B; kwargs...)
+        end
+
+        # copy documentation to both functions
+        Core.@__doc__ $f, $f!
+    end
+end
+
 """
     @functiondef [n_args=1] f
 
@@ -213,97 +302,7 @@ macro functiondef(args...)
     f isa Symbol || throw(ArgumentError("Unsupported usage of `@functiondef`"))
     f! = Symbol(f, :!)
 
-    # TODO is the right way?
-    @gensym A B
-    ex_single_arg = quote
-        # out of place to inplace
-        $f(A; kwargs...) = $f!(copy_input($f, A); kwargs...)
-        $f(A, alg::AbstractAlgorithm) = $f!(copy_input($f, A), alg)
-
-        # fill in arguments
-        function $f!(A; alg=nothing, kwargs...)
-            return $f!(A, select_algorithm($f!, A, alg; kwargs...))
-        end
-        function $f!(A, out; alg=nothing, kwargs...)
-            return $f!(A, out, select_algorithm($f!, A, alg; kwargs...))
-        end
-        function $f!(A, alg::AbstractAlgorithm)
-            return $f!(A, initialize_output($f!, A, alg), alg)
-        end
-
-        # define fallbacks for algorithm selection
-        @inline function select_algorithm(::typeof($f), A, alg::Alg; kwargs...) where {Alg}
-            return select_algorithm($f!, A, alg; kwargs...)
-        end
-        # define default algorithm fallbacks for out-of-place functions
-        # in terms of the corresponding in-place function
-        @inline function default_algorithm(::typeof($f), A; kwargs...)
-            return default_algorithm($f!, A; kwargs...)
-        end
-        # define default algorithm fallbacks for out-of-place functions
-        # in terms of the corresponding in-place function for types,
-        # in principle this is covered by the definition above but
-        # it is necessary to avoid ambiguity errors with the generic definitions:
-        # ```julia
-        # default_algorithm(f::F, A; kwargs...) where {F} = default_algorithm(f, typeof(A); kwargs...)
-        # function default_algorithm(f::F, ::Type{T}; kwargs...) where {F,T}
-        #     throw(MethodError(default_algorithm, (f, T)))
-        # end
-        # ```
-        @inline function default_algorithm(::typeof($f), ::Type{A}; kwargs...) where {A}
-            return default_algorithm($f!, A; kwargs...)
-        end
-
-        # copy documentation to both functions
-        Core.@__doc__ $f, $f!
-    end
-    ex_double_arg = quote
-        # out of place to inplace
-        $f(A, B; kwargs...) = $f!(copy_input($f, A, B)...; kwargs...)
-        $f(A, B, alg::AbstractAlgorithm) = $f!(copy_input($f, A, B)..., alg)
-
-        # fill in arguments
-        function $f!(A, B; alg=nothing, kwargs...)
-            return $f!(A, B, select_algorithm($f!, (A, B), alg; kwargs...))
-        end
-        function $f!(A, B, out; alg=nothing, kwargs...)
-            return $f!(A, B, out, select_algorithm($f!, (A, B), alg; kwargs...))
-        end
-        function $f!(A, B, alg::AbstractAlgorithm)
-            return $f!(A, B, initialize_output($f!, A, B, alg), alg)
-        end
-
-        # define fallbacks for algorithm selection
-        @inline function select_algorithm(::typeof($f), A, alg::Alg; kwargs...) where {Alg}
-            return select_algorithm($f!, A, alg; kwargs...)
-        end
-        # define default algorithm fallbacks for out-of-place functions
-        # in terms of the corresponding in-place function
-        @inline function default_algorithm(::typeof($f), A, B; kwargs...)
-            return default_algorithm($f!, A, B; kwargs...)
-        end
-        # define default algorithm fallbacks for out-of-place functions
-        # in terms of the corresponding in-place function for types,
-        # in principle this is covered by the definition above but
-        # it is necessary to avoid ambiguity errors with the generic definitions:
-        # ```julia
-        # default_algorithm(f::F, A; kwargs...) where {F} = default_algorithm(f, typeof(A); kwargs...)
-        # function default_algorithm(f::F, ::Type{T}; kwargs...) where {F,T}
-        #     throw(MethodError(default_algorithm, (f, T)))
-        # end
-        # ```
-        @inline function default_algorithm(::typeof($f), ::Type{A}, ::Type{B}; kwargs...) where {A, B}
-            return default_algorithm($f!, A, B; kwargs...)
-        end
-
-        # copy documentation to both functions
-        Core.@__doc__ $f, $f!
-    end
-    if f_n_args == 1
-        return esc(ex_single_arg)
-    elseif f_n_args == 2
-        return esc(ex_double_arg)
-    end
+    return esc(_arg_expr(Val(f_n_args), f, f!))
 end
 
 """
