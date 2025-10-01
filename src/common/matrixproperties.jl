b33aff15a02e47f55ce7377ac029a01a6451646e
Lukas Devos <ldevos98@gmail.com>
2025-10-01 15:03:46 -0400

    Add `ishermitian` and `isantihermitian`

--- src/common/matrixproperties.jl
+++ src/common/matrixproperties.jl
@@ -144,3 +144,67 @@ function _ishermitian_exact_offdiag(Al, Au, ::Val{anti}) where {anti}
     end
     return true
 end
+
+"""
+    ishermitian(A; isapprox_kwargs...)
+
+Test whether a linear map is Hermitian, i.e. `A = A'`.
+The `isapprox_kwargs` can be used to control the tolerances of the equality.
+"""
+function ishermitian(A; atol::Real = 0, rtol::Real = 0, kwargs...)
+    return (atol == rtol == 0) ? (A == A') : isapprox(A, A'; atol, rtol, kwargs...)
+end
+function ishermitian(A::AbstractMatrix; atol::Real = 0, rtol::Real = 0, norm = LinearAlgebra.norm, kwargs...)
+    Base.require_one_based_indexing(A)
+    m, n = size(A)
+    m == n || return false
+    if atol == rtol == 0
+        for j in 1:n
+            for i in 1:j
+                A[i, j] == adjoint(A[j, i]) || return false
+            end
+        end
+    elseif norm === LinearAlgebra.norm
+        atol = max(atol, rtol * norm(A))
+        for j in 1:n
+            for i in 1:j
+                isapprox(A[i, j], adjoint(A[j, i]); atol, abs, kwargs...)  || return false
+            end
+        end
+    else
+        return isapprox(A, A'; atol, rtol, norm, kwargs...)
+    end
+    return true
+end
+
+"""
+    isantihermitian(A; isapprox_kwargs...)
+
+Test whether a linear map is anti-Hermitian, i.e. `A = -A'`.
+The `isapprox_kwargs` can be used to control the tolerances of the equality.
+"""
+function isantihermitian(A; atol::Real = 0, rtol::Real = 0, kwargs...)
+    return (atol == 0 & rtol == 0) ? (A == -A') : isapprox(A, -A'; atol, rtol, kwargs...)
+end
+function isantihermitian(A::AbstractMatrix; atol::Real = 0, rtol::Real = 0, norm = LinearAlgebra.norm, kwargs...)
+    Base.require_one_based_indexing(A)
+    m, n = size(A)
+    m == n || return false
+    if atol == rtol == 0
+        @inbounds for j in 1:n
+            for i in 1:j
+                A[i, j] == -adjoint(A[j, i]) || return false
+            end
+        end
+    elseif norm === LinearAlgebra.norm
+        atol = max(atol, rtol * norm(A))
+        @inbounds for j in 1:n
+            for i in 1:j
+                isapprox(A[i, j], -adjoint(A[j, i]); atol, abs, kwargs...)  || return false
+            end
+        end
+    else
+        return isapprox(A, -A'; atol, rtol, norm, kwargs...)
+    end
+    return true
+end
