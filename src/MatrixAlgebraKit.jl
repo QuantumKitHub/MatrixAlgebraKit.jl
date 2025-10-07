d2252c0ce6e30d4ad7649149762b97d2ed6e7e23
Lukas Devos <ldevos98@gmail.com>
2025-10-07 13:01:33 -0400

    remove `opnorm` from imports

--- src/MatrixAlgebraKit.jl
+++ src/MatrixAlgebraKit.jl
@@ -4,7 +4,7 @@ using LinearAlgebra: LinearAlgebra
 using LinearAlgebra: norm # TODO: eleminate if we use VectorInterface.jl?
 using LinearAlgebra: mul!, rmul!, lmul!, adjoint!, rdiv!, ldiv!
 using LinearAlgebra: sylvester, lu!
-using LinearAlgebra: isposdef, issymmetric, opnorm
+using LinearAlgebra: isposdef, issymmetric
 using LinearAlgebra: Diagonal, diag, diagind, isdiag
 using LinearAlgebra: UpperTriangular, LowerTriangular
 using LinearAlgebra: BlasFloat, BlasReal, BlasComplex, BlasInt
