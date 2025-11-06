9afe7667255cfb06f7617b0ff23dedef2e0e2f64
Lukas Devos <ldevos98@gmail.com>
2025-11-06 10:46:13 -0500

    add testsuite macro

--- test/testsuite/TestSuite.jl
+++ test/testsuite/TestSuite.jl
@@ -8,4 +8,18 @@ Suite of tests that may be used for all packages inheriting from MatrixAlgebraKi
 """
 module TestSuite
 
+const tests = Dict()
+
+macro testsuite(name, ex)
+    safe_name = lowercase(replace(replace(name, " " => "_"), "/" => "_"))
+    fn = Symbol("test_", safe_name)
+    return quote
+        $(esc(fn))(AT; eltypes = supported_eltypes(AT, $(esc(fn)))) = $(esc(ex))(AT, eltypes)
+        @assert !haskey(tests, $name) "testsuite already exists"
+        tests[$name] = $fn
+    end
+end
+
+end
+
 end
