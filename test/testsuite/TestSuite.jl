6dc2081e02d09ceeb660b79653a2bc4280517572
Lukas Devos <ldevos98@gmail.com>
2025-11-06 10:47:14 -0500

    add various test utility

--- test/testsuite/TestSuite.jl
+++ test/testsuite/TestSuite.jl
@@ -8,6 +8,11 @@ Suite of tests that may be used for all packages inheriting from MatrixAlgebraKi
 """
 module TestSuite
 
+using Test, TestExtras
+using MatrixAlgebraKit
+using MatrixAlgebraKit: diagview
+using LinearAlgebra: norm, istriu
+
 const tests = Dict()
 
 macro testsuite(name, ex)
@@ -20,6 +25,25 @@ macro testsuite(name, ex)
     end
 end
 
+testargs_summary(args...) = string(args)
+
+instantiate_matrix(::Type{T}, size) where {T <: Number} = randn(T, size)
+instantiate_matrix(::Type{AT}, size) where {AT <: Array} = randn(eltype(AT), size)
+
+function has_positive_diagonal(A)
+    T = eltype(A)
+    return if T <: Real
+        all(≥(zero(T)), diagview(A))
+    else
+        all(≥(zero(real(T))), real(diagview(A))) &&
+            all(≈(zero(real(T))), imag(diagview(A)))
+    end
+end
+isleftnull(N, A; kwargs...) = isapprox(norm(N' * A), 0; kwargs...)
+
+# TODO: actually make this a test
+macro testinferred(ex)
+    return esc(:(@inferred $ex))
 end
 
 end
