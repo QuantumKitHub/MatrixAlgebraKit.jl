b7d9547306bf36e00afbae517a9cf893e4b59a36
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2025-12-10 14:38:18 +0100

    Don't use NaN because of BigFloat

--- src/implementations/svd.jl
+++ src/implementations/svd.jl
@@ -230,12 +230,12 @@ function svd_trunc!(A, USVᴴ::Tuple{TU, TS, TVᴴ}, alg::TruncatedAlgorithm; co
     Tr = real(eltype(A))
     ϵ = compute_error ? zeros(Tr, 1) : zeros(Tr, 0)
     (U, S, Vᴴ, ϵ) = svd_trunc!(A, (USVᴴ..., ϵ), alg)
-    return compute_error ? (U, S, Vᴴ, ϵ[1]) : (U, S, Vᴴ, Tr(NaN))
+    return compute_error ? (U, S, Vᴴ, ϵ[1]) : (U, S, Vᴴ, -one(Tr))
 end
 function svd_trunc!(A, USVᴴ::Nothing, alg::TruncatedAlgorithm; compute_error::Bool = true)
     ϵ = compute_error ? zeros(real(eltype(A)), 1) : zeros(real(eltype(A)), 0)
     U, S, Vᴴ, ϵ = svd_trunc!(A, (USVᴴ, ϵ), alg)
-    return compute_error ? (U, S, Vᴴ, ϵ[1]) : (U, S, Vᴴ, Tr(NaN))
+    return compute_error ? (U, S, Vᴴ, ϵ[1]) : (U, S, Vᴴ, -one(Tr))
 end
 
 # Diagonal logic
