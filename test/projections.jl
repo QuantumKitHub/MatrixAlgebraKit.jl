eb1eef80c0477b0db953dbbb07b3293462e4be3a
Olivier Gauthé <olivier.gauthe@quobly.io>
2025-12-17 16:58:21 +0100

    runic

--- test/projections.jl
+++ test/projections.jl
@@ -12,9 +12,9 @@ const BLASFloats = (Float32, Float64, ComplexF32, ComplexF64)
     m = 54
     noisefactor = eps(real(T))^(3 / 4)
 
-    mat0 = zeros(T, (1,1))
+    mat0 = zeros(T, (1, 1))
     @test ishermitian(mat0)
-    @test ishermitian(mat0; atol=default_hermitian_tol(mat0))
+    @test ishermitian(mat0; atol = default_hermitian_tol(mat0))
     @test isnothing(check_hermitian(mat0))
 
     for alg in (NativeBlocked(blocksize = 16), NativeBlocked(blocksize = 32), NativeBlocked(blocksize = 64))
