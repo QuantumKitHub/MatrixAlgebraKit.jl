74810317caef5bd65459716ea58255678d9fa59a
Katharine Hyatt <kshyatt@users.noreply.github.com>
2026-01-06 07:07:57 +0100

    Update test/testsuite/svd.jl
    Co-authored-by: Lukas Devos <ldevos98@gmail.com>

--- test/testsuite/svd.jl
+++ test/testsuite/svd.jl
@@ -138,7 +138,7 @@ function test_svd_full_algs(
         m, n = size(A)
         minmn = min(m, n)
 
-        U, S, Vᴴ = svd_full(A; alg)
+        U, S, Vᴴ = @testinferred svd_full(A; alg)
         @test size(U) == (m, m)
         @test eltype(S) == real(eltype(T)) && size(S) == (m, n)
         @test size(Vᴴ) == (n, n)
