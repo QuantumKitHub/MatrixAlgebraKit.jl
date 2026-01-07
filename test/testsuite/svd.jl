01b3d0d076810797f8a0b44420af04504df4cc06
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-01-07 10:19:11 -0500

    Try to unbreak AMDGPU

--- test/testsuite/svd.jl
+++ test/testsuite/svd.jl
@@ -167,7 +167,8 @@ function test_svd_trunc(
             U1, S1, V1ᴴ, ϵ1 = @testinferred svd_trunc(A; trunc = truncrank(r))
             @test length(diagview(S1)) == r
             @test collect(diagview(S1)) ≈ S₀[1:r]
-            @test opnorm(A - U1 * S1 * V1ᴴ) ≈ S₀[r + 1]
+            AUSV_vals = svd_vals(A - U1 * S1 * V1ᴴ) # bypass broken svdvals on AMDGPU
+            @test opnorm(Diagonal(AUSV_vals)) ≈ S₀[r + 1]
             # Test truncation error
             @test ϵ1 ≈ norm(view(S₀, (r + 1):minmn)) atol = atol
 
@@ -248,7 +249,8 @@ function test_svd_trunc_algs(
             U1, S1, V1ᴴ, ϵ1 = @testinferred svd_trunc(A; trunc = truncrank(r), alg)
             @test length(diagview(S1)) == r
             @test collect(diagview(S1)) ≈ S₀[1:r]
-            @test opnorm(A - U1 * S1 * V1ᴴ) ≈ S₀[r + 1]
+            AUSV_vals = svd_vals(A - U1 * S1 * V1ᴴ) # bypass broken svdvals on AMDGPU
+            @test opnorm(Diagonal(AUSV_vals)) ≈ S₀[r + 1]
             # Test truncation error
             @test ϵ1 ≈ norm(view(S₀, (r + 1):minmn)) atol = atol
 
