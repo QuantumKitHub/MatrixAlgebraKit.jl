eba9345fd59e59bef4f4056cc1bc04a0eb01a86a
Lukas Devos <ldevos98@gmail.com>
2026-01-15 11:32:00 +0100

    Revert "format?"
    This reverts commit 2fc27860567156cc559ed12cef86ee29fc2e43c5.

--- src/interface/orthnull.jl
+++ src/interface/orthnull.jl
@@ -431,9 +431,9 @@ left_orth_alg(alg::AbstractAlgorithm) = error(
     """
     Unknown or invalid `left_orth` algorithm type `$(typeof(alg))`.
     To register the algorithm type for `left_orth`, define
-    
+
         MatrixAlgebraKit.left_orth_alg(alg::CustomAlgorithm) = LeftOrthAlgorithm{kind}(alg)
-    
+
     where `kind` selects the factorization type that will be used.
     By default, this is either `:qr`, `:polar` or `:svd`, to select [`qr_compact!`](@ref),
     [`left_polar!`](@ref), [`svd_compact!`](@ref) or [`svd_trunc!`](@ref) respectively.
@@ -466,9 +466,9 @@ right_orth_alg(alg::AbstractAlgorithm) = error(
     """
     Unknown or invalid `right_orth` algorithm type `$(typeof(alg))`.
     To register the algorithm type for `right_orth`, define
-    
+
         MatrixAlgebraKit.right_orth_alg(alg::CustomAlgorithm) = RightOrthAlgorithm{kind}(alg)
-    
+
     where `kind` selects the factorization type that will be used.
     By default, this is either `:lq`, `:polar` or `:svd`, to select [`lq_compact!`](@ref),
     [`right_polar!`](@ref), [`svd_compact!`](@ref) or [`svd_trunc!`](@ref) respectively.
@@ -501,9 +501,9 @@ left_null_alg(alg::AbstractAlgorithm) = error(
     """
     Unknown or invalid `left_null` algorithm type `$(typeof(alg))`.
     To register the algorithm type for `left_null`, define
-    
+
         MatrixAlgebraKit.left_null_alg(alg::CustomAlgorithm) = LeftNullAlgorithm{kind}(alg)
-    
+
     where `kind` selects the factorization type that will be used.
     By default, this is either `:qr` or `:svd`, to select [`qr_null!`](@ref),
     [`svd_compact!`](@ref) or [`svd_trunc!`](@ref) respectively.
@@ -535,9 +535,9 @@ right_null_alg(alg::AbstractAlgorithm) = error(
     """
     Unknown or invalid `right_null` algorithm type `$(typeof(alg))`.
     To register the algorithm type for `right_null`, define
-    
+
         MatrixAlgebraKit.right_null_alg(alg::CustomAlgorithm) = RightNullAlgorithm{kind}(alg)
-    
+
     where `kind` selects the factorization type that will be used.
     By default, this is either `:lq` or `:svd`, to select [`lq_null!`](@ref),
     [`svd_compact!`](@ref) or [`svd_trunc!`](@ref) respectively.
