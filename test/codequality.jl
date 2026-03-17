1575f6fb6cf87bcd93a1b9be83439d1998b612b4
Katharine Hyatt <kshyatt@users.noreply.github.com>
2026-03-17 16:14:33 +0100

    Disable JET for now (#191)

--- test/codequality.jl
+++ test/codequality.jl
@@ -3,5 +3,6 @@ using MatrixAlgebraKit
 using Aqua
 Aqua.test_all(MatrixAlgebraKit)
 
-using JET
-JET.test_package(MatrixAlgebraKit; target_defined_modules = true)
+# using JET
+# JET.test_package(MatrixAlgebraKit; target_defined_modules = true)
+# disable for now (see https://github.com/QuantumKitHub/MatrixAlgebraKit.jl/pull/188#pullrequestreview-3953586976)
