7e1074acec406db2e52e3e110b31e2049795c4ee
lkdvos <ldevos98@gmail.com>
2026-03-19 15:25:50 -0400

    expand docstring

--- src/interface/decompositions.jl
+++ src/interface/decompositions.jl
@@ -307,9 +307,21 @@ until convergence up to tolerance `tol`.
 """
     DefaultAlgorithm(; kwargs...)
 
-Algorithm sentinel that resolves to the default algorithm for a given function and input
-type at call time. Any keyword arguments stored in the instance are forwarded at runtime
-to [`select_algorithm`](@ref).
+Algorithm sentinel that resolves to the algorithm selection procedure for a given function and input type at call time.
+This provides a unified approach for package developers to store both keyword argument and direct algorithm inputs.
+Any keyword arguments stored in the instance are forwarded at runtime to [`select_algorithm`](@ref).
+
+For example, the following calls are equivalent:
+
+```julia
+A = rand(3, 3)
+
+# specifying keyword arguments
+Q, R = qr_compact(A; positive = true)
+
+# wrapping keyword arguments in DefaultAlgorithm
+alg = DefaultAlgorithm(; positive = true)
+Q, R = qr_compact(A; alg)
 """
 @algdef DefaultAlgorithm
 
