399b2a2d6ed6b599378d1af5714d79c45a6931f6
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-05-20 10:05:57 -0400

    Move atol and rtol

--- test/mooncake/polar.jl
+++ test/mooncake/polar.jl
@@ -13,8 +13,8 @@ is_buildkite = get(ENV, "BUILDKITE", "false") == "true"
 m = 19
 for T in (BLASFloats..., GenericFloats...), n in (17, m, 23)
     TestSuite.seed_rng!(1234)
+    atol = rtol = m * n * TestSuite.precision(T)
     if !is_buildkite
-        atol = rtol = m * n * TestSuite.precision(T)
         m >= n && TestSuite.test_mooncake_left_polar(T, (m, n); atol, rtol)
         n >= m && TestSuite.test_mooncake_right_polar(T, (m, n); atol, rtol)
         #=if m == n
