a19ea21ebd3570e9d4f6271a54ca8b3063e4537e
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-06-03 06:44:42 -0400

    Comments and fix

--- src/pushforwards/polar.jl
+++ src/pushforwards/polar.jl
@@ -1,11 +1,11 @@
 function left_polar_pushforward!(ΔA, A, WP, ΔWP; kwargs...)
     W, P = WP
     ΔW, ΔP = ΔWP
-    aWdA = adjoint(W) * ΔA
-    K̇ = _sylvester(P, P, -(aWdA - adjoint(aWdA)))
-    L̇ = (Diagonal(ones(eltype(W), size(W, 1))) - W * adjoint(W)) * ΔA * inv(P)
+    WᴴdA = adjoint(W) * ΔA
+    K̇ = _sylvester(P, P, -(WᴴdA - adjoint(WᴴdA)))
+    L̇ = (LinearAlgebra.UniformScaling(1) - W * adjoint(W)) * ΔA * inv(P)
     ΔW .= W * K̇ + L̇
-    ΔP .= aWdA - K̇ * P
+    ΔP .= WᴴdA - K̇ * P
     return (ΔW, ΔP)
 end
 
@@ -14,7 +14,7 @@ function right_polar_pushforward!(ΔA, A, PWᴴ, ΔPWᴴ; kwargs...)
     ΔP, ΔWᴴ = ΔPWᴴ
     dAW = ΔA * adjoint(Wᴴ)
     K̇ = _sylvester(P, P, -(dAW - adjoint(dAW)))
-    L̇ = inv(P) * ΔA * (Diagonal(ones(eltype(Wᴴ), size(Wᴴ, 2))) - adjoint(Wᴴ) * Wᴴ)
+    L̇ = inv(P) * ΔA * (LinearAlgebra.UniformScaling(1) - adjoint(Wᴴ) * Wᴴ)
     ΔWᴴ .= K̇ * Wᴴ + L̇
     ΔP .= dAW - P * K̇
     return (ΔWᴴ, ΔP)
