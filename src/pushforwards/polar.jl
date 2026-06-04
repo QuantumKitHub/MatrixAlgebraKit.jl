887b1cb8fd48bc4c48df069d0d338db9daa84f0a
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-06-04 02:24:06 -0400

    Pushforward improvements

--- src/pushforwards/polar.jl
+++ src/pushforwards/polar.jl
@@ -1,25 +1,25 @@
 function left_polar_pushforward!(ΔA, A, WP, ΔWP; kwargs...)
     W, P = WP
     ΔW, ΔP = ΔWP
-    WᴴdA = adjoint(W) * ΔA
-    K̇ = _sylvester(P, P, -(WᴴdA - adjoint(WᴴdA)))
+    mul!(ΔP, adjoint(W), ΔA, +1, 0)
+    K̇ = _sylvester(P, P, adjoint(ΔP) - ΔP)
     dAiP = ΔA * inv(P)
     WᴴdAiP = W' * dAiP
     L̇ = mul!(dAiP, W, WᴴdAiP, -1, +1)
     ΔW .= W * K̇ + L̇
-    ΔP .= WᴴdA - K̇ * P
+    ΔP = mul!(ΔP, K̇, P, -1, +1)
     return (ΔW, ΔP)
 end
 
 function right_polar_pushforward!(ΔA, A, PWᴴ, ΔPWᴴ; kwargs...)
     P, Wᴴ = PWᴴ
     ΔP, ΔWᴴ = ΔPWᴴ
-    dAW = ΔA * adjoint(Wᴴ)
-    K̇ = _sylvester(P, P, -(dAW - adjoint(dAW)))
+    mul!(ΔP, ΔA, adjoint(Wᴴ), +1, 0)
+    K̇ = _sylvester(P, P, adjoint(ΔP) - ΔP)
     iPdA = inv(P) * ΔA
     iPdAW = iPdA * Wᴴ'
     L̇ = mul!(iPdA, iPdAW, Wᴴ, -1, +1)
     ΔWᴴ .= K̇ * Wᴴ + L̇
-    ΔP .= dAW - P * K̇
+    ΔP = mul!(ΔP, P, K̇, -1, +1)
     return (ΔWᴴ, ΔP)
 end
