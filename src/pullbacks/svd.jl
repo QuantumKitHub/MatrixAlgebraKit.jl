84c9f8a488bce9c249accf7f61c550dd327552a3
Katharine Hyatt <kshyatt@users.noreply.github.com>
2026-09-16 16:32:08 +0200

    Fix stupid typo

--- src/pullbacks/svd.jl
+++ src/pullbacks/svd.jl
@@ -69,7 +69,7 @@ function check_and_prepare_svd_cotangents(
                     mul!(vtmp, wtmp, V₁ᴴ, -1, 1)
                     ΔgaugeV = max(ΔgaugeV, norm(vtmp))
                 else # remaining rows should be zero
-                    ΔgaugeV = max(ΔgaugeV, maximum(abs, view(ΔVᴴ, j, :); init = abs(zero(eltype(ΔVᴴ))))))
+                    ΔgaugeV = max(ΔgaugeV, maximum(abs, view(ΔVᴴ, j, :); init = abs(zero(eltype(ΔVᴴ)))))
                 end
             end
         end
